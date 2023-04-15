33b4301019ef33e00a197b8f96564eadfdc1da31
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-15 13:49:30 +0300

    Chain of responsibilities

---
+++ Ланцюжок обов'язків/Ланцюжок обов'язків/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ланцюжок_обов_язків
+{
+    internal class Class1
+    {
+        public interface IAutoAssemblyHandler
+        {
+            IAutoAssemblyHandler SetNext(IAutoAssemblyHandler handler);
+            string Handle(string request);
+        }
+
+        public abstract class AutoAssemblyHandlerBase : IAutoAssemblyHandler
+        {
+            private IAutoAssemblyHandler _nextHandler;
+
+            public IAutoAssemblyHandler SetNext(IAutoAssemblyHandler handler)
+            {
+                _nextHandler = handler;
+                return handler;
+            }
+
+            public virtual string Handle(string request)
+            {
+                if (_nextHandler != null)
+                {
+                    return _nextHandler.Handle(request);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public class BodyAssemblyHandler : AutoAssemblyHandlerBase
+        {
+            public override string Handle(string request)
+            {
+                if (request.Contains("Кузов"))
+                {
+                    return "Кузов зібрано. ";
+                }
+                else
+                {
+                    return base.Handle(request);
+                }
+            }
+        }
+
+        public class EngineAssemblyHandler : AutoAssemblyHandlerBase
+        {
+            public override string Handle(string request)
+            {
+                if (request.Contains("Двигун"))
+                {
+                    return "Двигун зібрано. ";
+                }
+                else
+                {
+                    return base.Handle(request);
+                }
+            }
+        }
+
+        public class TransmissionAssemblyHandler : AutoAssemblyHandlerBase
+        {
+            public override string Handle(string request)
+            {
+                if (request.Contains("Трансмісія"))
+                {
+                    return "Трансмісія зібрана. ";
+                }
+                else
+                {
+                    return base.Handle(request);
+                }
+            }
+        }
+
+        public class AutoAssemblyLine
+        {
+            private readonly BodyAssemblyHandler _bodyAssemblyHandler = new BodyAssemblyHandler();
+            private readonly EngineAssemblyHandler _engineAssemblyHandler = new EngineAssemblyHandler();
+            private readonly TransmissionAssemblyHandler _transmissionAssemblyHandler = new TransmissionAssemblyHandler();
+
+            public AutoAssemblyLine()
+            {
+                _bodyAssemblyHandler.SetNext(_engineAssemblyHandler).SetNext(_transmissionAssemblyHandler);
+            }
+
+            public string AssembleAuto(string request)
+            {
+                return _bodyAssemblyHandler.Handle(request);
+            }
+        }
+    }
+}
---
+++ Ланцюжок обов'язків/Ланцюжок обов'язків/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Ланцюжок_обов_язків.Class1;
+
+namespace Ланцюжок_обов_язків
+{
+    public partial class Form1 : Form
+    {
+        private readonly AutoAssemblyLine _assemblyLine = new AutoAssemblyLine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string request = textBox1.Text;
+            string result = _assemblyLine.AssembleAuto(request);
+            MessageBox.Show(result, "Assembly Result");
+        }
+    }
+}
